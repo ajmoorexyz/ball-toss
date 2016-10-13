50a14b751401055fb2984c57d5b8e62424fa234f
Alex Moore <alex.moore@workday.com>
2016-10-12 19:58:23 -0600

    adds node server with min functionality
    * restructures files
    * leverages tsc for transpilation

---
+++ dist/main.js
@@ -0,0 +1,2 @@
+// all .ts files will inadvertantly be imported in this file
+var BALL_TOSS = function () { console.log('ball toss'); };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,2 @@
 // all .ts files will inadvertantly be imported in this file
+const BALL_TOSS = () => {console.log('ball toss')}
