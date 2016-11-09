8fbc6e6e5bb56106bd7a408500b9e6e2feee363e
Alex Moore <alex.moore@workday.com>
2016-11-09 07:58:39 -0700

    feature: adds a toggle to ensure the same message is not emitted two times in a row
    TODO: this will wind up being an implementation of STATE
    also, users will be limited to (2) initially

--- public/main.js
+++ public/main.js
@@ -1,7 +1,15 @@
 var socket = io()
 
+// 'toss' is emitted when the submit
+// btn is clicked (tossed)
 $('form').submit(function() {
-  socket.emit('toss', $('#m').val())
-  $('#m').val('')
+  socket.emit('toss', $('.ball').val())
+  toggle($('.ball').val())
   return false
 })
+
+// this toggle is meant to abstract
+// the business logic from the view
+function toggle(a) {
+  a === 'Catch' ? $('.ball').val('Toss') : $('.ball').val('Catch')
+}
